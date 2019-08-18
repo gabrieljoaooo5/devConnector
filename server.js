f0eaeaa57365de2ef06cfd0b5124fa74799bc56c
João Gabriel Andrade <joaogabriel1598@gmail.com>
2019-08-18 12:03:26 -0300

    Express & MongoDB Setup

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect DB
+connectDB();
+
+//Define Route
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 app.get('/', (req, res) => res.send('API Running'));
 
